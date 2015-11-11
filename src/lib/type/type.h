00554462bdbb981f1b32d3dd00b2f08360cb344a
madwyn <madwyn@gmail.com>
2015-11-11 11:33:25 +0000

    extend enum <-> string serialisation

--- src/lib/type/type.h
+++ src/lib/type/type.h
@@ -20,6 +20,19 @@ inline int STRING_TO_ENUM_STARTS(const char *enum_name, const char ** str_arr, i
 }
 
 /**
+ * convert string name to corresponding enum when starts with
+ */
+inline int STRING_TO_ENUM_CONTAINS(const char *enum_name, const char ** str_arr, int str_num) {
+    for (int i = 0; i < str_num; ++ i) {
+        if (strstr(enum_name, str_arr[i])) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+/**
  * convert string name to corresponding enum when matches
  */
 inline int STRING_TO_ENUM(const char *enum_name, const char ** str_arr, int str_num) {
