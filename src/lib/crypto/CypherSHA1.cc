47e77e400e9cdef108da428bc9120b35ed735263
madwyn <madwyn@gmail.com>
2015-11-19 18:11:08 +0000

    fix SHA1 decryption

--- src/lib/crypto/CypherSHA1.cc
+++ src/lib/crypto/CypherSHA1.cc
@@ -9,13 +9,16 @@ namespace fw {
 #define SHA1_DIGEST_LEN	20
 #endif
 
+
 MSG_CODE CypherSHA1::enc(const uint8_t * const input, uint8_t *output, const size_t len) {
     return crypt(input, output, len);
 }
 
+
 MSG_CODE CypherSHA1::dec(const uint8_t * const input, uint8_t *output, const size_t len) {
     return crypt(input, output, len);
 }
+
 
 MSG_CODE CypherSHA1::crypt(const uint8_t *const input, uint8_t *output, const size_t len) {
     static const uint8_t seed_1[SHA1_DIGEST_LEN] = {
@@ -31,32 +34,35 @@ MSG_CODE CypherSHA1::crypt(const uint8_t *const input, uint8_t *output, const si
     };
 
     mbedtls_sha1_context ctx;
+    mbedtls_sha1_init(&ctx);
 
     // set key
     mbedtls_sha1_starts(&ctx);
     mbedtls_sha1_update(&ctx, seed_1, SHA1_DIGEST_LEN);
     mbedtls_sha1_update(&ctx, seed_2, SHA1_DIGEST_LEN);
 
-    auto _crypt = [&ctx](const uint8_t *const in, uint8_t *out) {
+    uint8_t sha_digest[SHA1_DIGEST_LEN];
+
+    auto _crypt = [&ctx, &sha_digest](const uint8_t *const in, uint8_t *out) {
         // get the latest sha-1 digest (result used to xor data buffer)
-        mbedtls_sha1_finish(&ctx, out);
+        mbedtls_sha1_finish(&ctx, sha_digest);
 
         // use the newly calculated digest, along with "Decrypt_sha1_seed_2"
         // to start a new sha-1 calculation (resulting digest will be used
         // next time through this loop)
         mbedtls_sha1_starts(&ctx);
-        mbedtls_sha1_update(&ctx, out   , SHA1_DIGEST_LEN);
-        mbedtls_sha1_update(&ctx, seed_2, SHA1_DIGEST_LEN);
+        mbedtls_sha1_update(&ctx, sha_digest, SHA1_DIGEST_LEN);
+        mbedtls_sha1_update(&ctx, seed_2,     SHA1_DIGEST_LEN);
 
         const uint8_t *p_in = in;
 
         // now take the sha-1 digest (from above) and xor that into
         // the next 20 bytes of the data buffer
         for (size_t i = 0; i < SHA1_DIGEST_LEN; i++) {
-            *out++ = (*p_in++ ^ out[i]);
+            *out++ = (*p_in++ ^ sha_digest[i]);
         }
 
-        return 1;
+        return 0;
     };
 
     return _crypt_buf(input, output, len, SHA1_DIGEST_LEN, _crypt);
