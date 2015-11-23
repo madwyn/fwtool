6831c8817c7c6aa1cedec7d4f14ae5fd6e340cd2
madwyn <madwyn@gmail.com>
2015-11-23 10:37:16 +0000

    type conversion for cargo enum parameters

--- src/cmd/main.c
+++ src/cmd/main.c
@@ -25,7 +25,7 @@ int main(int argc, char **argv) {
     // flag context
     cargo_t cargo;
 
-    if (cargo_init(&cargo, 0, "%s", argv[0])) {
+    if (cargo_init(&cargo, (cargo_flags_t)0, "%s", argv[0])) {
         fprintf(stderr, "Failed to init command line parsing\n");
         return -1;
     }
@@ -35,19 +35,19 @@ int main(int argc, char **argv) {
     // working mode group
     ret |= cargo_add_mutex_group(cargo, CARGO_MUTEXGRP_ONE_REQUIRED, OPT_GRP_MODE, "Working Mode", "The working mode");
 
-    ret |= cargo_add_option(cargo, 0, OPT_MODE_ADV_HELP_F, MSG_MODE_ADV_HELP, "b", &adv_help);
-    ret |= cargo_add_option(cargo, 0, OPT_MODE_EXTRACT_F , MSG_MODE_EXTRACT , "b", &extract);
-    ret |= cargo_add_option(cargo, 0, OPT_MODE_CREATE_F  , MSG_MODE_CREATE  , "b", &create);
+    ret |= cargo_add_option(cargo, (cargo_option_flags_t)0, OPT_MODE_ADV_HELP_F, MSG_MODE_ADV_HELP, "b", &adv_help);
+    ret |= cargo_add_option(cargo, (cargo_option_flags_t)0, OPT_MODE_EXTRACT_F , MSG_MODE_EXTRACT , "b", &extract);
+    ret |= cargo_add_option(cargo, (cargo_option_flags_t)0, OPT_MODE_CREATE_F  , MSG_MODE_CREATE  , "b", &create);
 
     ret |= cargo_mutex_group_add_option(cargo, OPT_GRP_MODE, OPT_MODE_ADV_HELP);
     ret |= cargo_mutex_group_add_option(cargo, OPT_GRP_MODE, OPT_MODE_EXTRACT);
     ret |= cargo_mutex_group_add_option(cargo, OPT_GRP_MODE, OPT_MODE_CREATE);
 
     // input parameters
-    ret |= cargo_add_group(cargo, 0, OPT_GRP_IO, "Input and output parameters", "They have different meanings depending on working mode.");
+    ret |= cargo_add_group(cargo, (cargo_group_flags_t)0, OPT_GRP_IO, "Input and output parameters", "They have different meanings depending on working mode.");
 
-    ret |= cargo_add_option(cargo, 0, OPT_IN_F , MSG_IO_IN , "s", &input);
-    ret |= cargo_add_option(cargo, 0, OPT_OUT_F, MSG_IO_OUT, "s", &output);
+    ret |= cargo_add_option(cargo, (cargo_option_flags_t)0, OPT_IN_F , MSG_IO_IN , "s", &input);
+    ret |= cargo_add_option(cargo, (cargo_option_flags_t)0, OPT_OUT_F, MSG_IO_OUT, "s", &output);
 
     ret |= cargo_group_add_option(cargo, OPT_GRP_IO, OPT_IN);
     ret |= cargo_group_add_option(cargo, OPT_GRP_IO, OPT_OUT);
@@ -55,14 +55,14 @@ int main(int argc, char **argv) {
     // optional parameters
     ret |= cargo_add_group(cargo, CARGO_GROUP_HIDE, OPT_GRP_OPTION, "Optional", "These are optional.");
 
-    ret |= cargo_add_option(cargo, 0, OPT_VERBOSE_F, MSG_OPT_VERBOSE, "b", &verbose);
+    ret |= cargo_add_option(cargo, (cargo_option_flags_t)0, OPT_VERBOSE_F, MSG_OPT_VERBOSE, "b", &verbose);
 
     ret |= cargo_group_add_option(cargo, OPT_GRP_OPTION, OPT_VERBOSE);
 
-    if (!cargo_parse(cargo, 0, 1, argc, argv)) {
+    if (!cargo_parse(cargo, (cargo_flags_t)0, 1, argc, argv)) {
         if (adv_help) {
             // Remove the group hide flag so it shows up in the usage.
-            cargo_group_set_flags(cargo, OPT_GRP_OPTION, 0);
+            cargo_group_set_flags(cargo, OPT_GRP_OPTION, (cargo_group_flags_t)0);
             cargo_print_usage(cargo, CARGO_USAGE_FULL);
         } else if (extract) {
             // implement extract
